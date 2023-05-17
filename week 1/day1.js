86bedb2c9f77c20bb980c90006c4f20c76d152f5
ericlombo <ericlombo1999@gmail.com>
2023-05-17 12:09:51 +0300

    modified

--- week 1/day1.js
+++ week 1/day1.js
@@ -10,3 +10,26 @@
     //name; 'pinchez', location; 'japan', age; '22', occupation; 'janitor'
    //)
 //}
+
+
+//the program should:
+
+//Prompt the user to enter the length and width of the rectangle.
+//Calculate the area and perimeter of the rectangle using the length and width entered by the user.
+let userNum1 = prompt('enter your first number')
+let userNum2 =prompt('enter your second number')
+
+let convertedNum1 = parseInt(userNum1)
+let convertedNum2 = parseInt(userNum2)
+
+function area (num1, num2){
+console.log(num1 * num2)
+
+}
+
+function perimeter (num1,num2){
+    console.log(num1 + num2)
+}
+
+area (convertedNum1, convertedNum2)
+perimeter (convertedNum1, convertedNum2)
